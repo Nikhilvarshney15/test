7bfc033b38297875c3d5af01952ae53b806fe2bf
Nikhilvarshney15 <59953511+Nikhilvarshney15@users.noreply.github.com>
2020-01-16 15:34:15 +0530

    Add files via upload

---
+++ test2.php
@@ -0,0 +1,98 @@
+<html>
+<head>
+<title>for preparation</title>
+<link rel="stylesheet" href="css/test2.css">
+		 
+
+</head>
+<body>
+<div class="top">
+<form>
+  <input type="radio" name="gender" value="male" checked> one way
+  <input type="radio" name="gender" value="female"> round trip<br>
+
+</form>
+</div>
+<div class="middle">
+<div class="hostel1">
+  
+   </div>
+  <p> fare calender</p>
+   <div class="hostel2">
+  
+   </div> 
+   <p>round trip offer</p>
+   <div class="number">
+  
+   </div> 
+   <div class="number">
+  
+   </div> 
+   <div class="number">
+  
+   </div> 
+   <div class="number">
+  
+   </div> 
+   <div class="number">
+  
+   </div> 
+   <div class="text">
+  <p> sort by</p>
+   
+   <div class="number2">
+  
+   </div> 
+   <div class="number3">
+  
+   </div> 
+   <div class="number3">
+  
+   </div> 
+    <div class="number3">
+  
+   </div> 
+    <div class="number3">
+  
+   </div> 
+    <div class="number3">
+  
+   </div> 
+    <div class="number3">
+  
+   </div> 
+    <div class="number3">
+  
+   </div> 
+   </div>
+   <div class="text">
+  <p> sort by</p>
+   
+   <div class="number2">
+  
+   </div>
+ <div class="number3">
+  
+   </div> 
+ <div class="number3">
+  
+   </div> 
+ <div class="number3">
+  
+   </div> 
+ <div class="number3">
+  
+   </div> 
+ <div class="number3">
+  
+   </div> 
+ <div class="number3">
+  
+   </div> 
+ <div class="number3">
+  
+   </div>    
+   </div>
+</div>
+</body>
+</html>
